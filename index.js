161e51ca56e38838571ea301af6a6c8ae33a0590
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 15:08:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
 function lowerCaseDrivers(list){
   return list.map( (ele) => { return ele.toLowerCase() })
 }
+/*
+a = ('Bobby', 'Sammy', 'Sally')
+a[0].toLowerCase = 'bobby'
+a[1].toLowerCase = 'sammy'
+a[2].toLowerCase = 'sally'
+*/
+
+
 function nameToAttributes(list){
   return list.map( (ele) => {
     let names = ele.split(' ');
     return {'firstName':names[0], 'lastName': names[1]}
    })
 }
+/*
+a = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins']
+a[0].split(' ') = ['Bobby', 'Smith']
+a[1].split(' ') = 'Sammy Watkins'
+*/
 function attributesToPhrase(list){
   return list.map( (ele) => {
     let info = ele.split(' is from ')
     return {'name': info[0], 'hometown':info[1]}
   })
 }
+/*
+  
+*/
