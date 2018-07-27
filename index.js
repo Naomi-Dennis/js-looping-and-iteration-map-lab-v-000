88bfd12bc96ca9fd4640984ce2f497d36e27d02d
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 15:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Code your solution in this file.
 function lowerCaseDrivers(list){
-  return list.map( (ele) => {ele.toLowerCase() })
+  return list.map( (ele) => { return ele.toLowerCase() })
+}
+function nameToAttributes(list){
+  return list.map( (ele) => {
+    let names = ele.split(' ');
+    returnn {'firstName':names[0], 'lastName': names[1]}
+   })
 }
